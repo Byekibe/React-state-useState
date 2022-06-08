5236b8ca0679cc43d046799c3a4f0c10b58d67ba
Byekibe <kibetpeter95@gmail.com>
2022-06-08 15:24:20 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [state, setState] = useState(0)
+
+function addition() {
+	setState(() => state+1)
+	}
+
+function subtraction() {
+	setState(() => state-1)
+}
+
+  return(
+  <div>
+	  <button onClick={subtraction}>-</button>{state}<button onClick={addition}>+</button>
+  </div>
+     );
 }
 
 export default App;
